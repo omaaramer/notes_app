b3b1a8162bf19c52162fbbaaee71897fbbb00de0
Omar Amer <omahar1907@gmail.com>
2024-02-06 01:42:33 +0200

    creat custom appBar

---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          'Notes',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            CustomAppBar(),
+          ],
+        ),
+      ),
+    );
+  }
+}
