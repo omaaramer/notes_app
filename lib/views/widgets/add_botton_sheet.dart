a315b32d9d3530d40ae9b3153a653d917727e9a8
Omar Amer <omahar1907@gmail.com>
2024-02-06 05:15:25 +0200

    finish text bottom sheet

--- lib/views/widgets/add_botton_sheet.dart
+++ lib/views/widgets/add_botton_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_button.dart';
 
 import 'custom_text_field.dart';
 
@@ -11,18 +12,23 @@ class AddModelBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTetField(
-            hint: 'Title',
-          ),
-          SizedBox(height: 16),
-          CustomTetField(
-            hint: 'Content',
-            maxLine: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            CustomTetField(
+              hint: 'Title',
+            ),
+            SizedBox(height: 16),
+            CustomTetField(
+              hint: 'Content',
+              maxLine: 5,
+            ),
+            SizedBox(height: 32),
+            CustomButtun(text: "Add"),
+            SizedBox(height: 16),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/views/widgets/custom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomButtun extends StatelessWidget {
+  const CustomButtun({super.key, required this.text, this.onTap});
+  final String text;
+  final VoidCallback? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          height: 50,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: kPrimaryColor,
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          )),
+    );
+  }
+}
